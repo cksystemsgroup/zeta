bbeeae5b832bcd4ab5935a6ac935e0ab3bd3b9fb
Andreas Haas <dra.haas@gmail.com>
2013-03-14 14:30:18 +0100

    header added to all source files

--- cpp/analyzer/analyzer.h
+++ cpp/analyzer/analyzer.h
@@ -1,3 +1,6 @@
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #ifndef ANALYZER_H
 #define ANALYZER_H
 
--- cpp/analyzer/test.cpp
+++ cpp/analyzer/test.cpp
@@ -1,3 +1,6 @@
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #define __STDC_FORMAT_MACROS
 #include <stdio.h>
 #include "operation.h"
