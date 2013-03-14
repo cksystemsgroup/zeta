3f1874ccda454e1a87639759f4539d023e0a0993
Andreas Haas <dra.haas@gmail.com>
2013-03-14 14:54:24 +0100

    added header to all source files

--- cpp/verifier/fifoExecuterUpperBound.h
+++ cpp/verifier/fifoExecuterUpperBound.h
@@ -1,3 +1,6 @@
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #ifndef FIFO_EXECUTER_UPPER_BOUND_H
 #define FIFO_EXECUTER_UPPER_BOUND_H
 
