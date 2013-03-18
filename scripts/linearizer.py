3869c90d632ea611770c1df3f9b541d3f2f4a01b
Andreas Haas <dra.haas@gmail.com>
2013-03-18 12:20:03 +0100

    new master script to calculate linearizations

--- scripts/linearizer.py
+++ scripts/linearizer.py
@@ -20,27 +20,27 @@ def getLineCount(filename):
 def startCalculation(inputData):
 
   item = inputData["filename"]
-  filename = ""
+  outputFile = ""
   lines = getLineCount(item)
   if lines > 0:
 
-    filename = "{directory}{filename}".format(filename = os.path.basename(item), directory = inputData["directory"])
-    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+    outputFile = "{directory}{filename}".format(filename = os.path.basename(item), directory = inputData["directory"])
+    if os.path.exists(outputFile) and os.path.getsize(outputFile) > 0:
       return
   else :
     print ("File {filename} is empty or does not exist.".format(filename=item))
     return
 
-  resultFile = open(filename, 'wb')
-  print ("{item}; {resultfile}".format(item = item, resultfile = filename))
-  analyzer = subprocess.Popen(["../cpp/analyzer/linearizer", "sum", str(lines), item], stdout=resultFile)
+  resultFile = open(outputFile, 'wb')
+  print ("{item}; {resultfile}".format(item = item, resultfile = outputFile))
+  analyzer = subprocess.Popen(["../cpp/analyzer/linearizer", inputData["mode"], str(lines), item], stdout=resultFile)
   analyzer.wait()
-  # resultFile.close()
+  resultFile.close()
   if analyzer.returncode and analyzer.returncode != 0 :
     print ("The analyzer failed for the file {filename}".format(filename = item))
     return
 
-def calcLinearization(inputDir, outputDir, mode):
+def calcLinearization(mode, inputDir, outputDir):
  
   if not inputDir.endswith('/') :
     inputDir = inputDir + '/'
@@ -63,7 +63,7 @@ def calcLinearization(inputDir, outputDir, mode):
 #  startCalculation(filenames[0])
 #  pool.map(startCalculation, [1, 2, 3])
 
-if len(sys.argv) < 2:
-  print ("python op_fairness.py <directory>, e.g. 'python op_fairness pldi13/' for logfiles in /mnt/local_homes/ahaas/pldi13/")
+if len(sys.argv) < 4:
+  print ("python linearizer.py <mode> <inputDir> <outputDir>, e.g. 'python linearizer.py max logfiles/ linearizations/'")
 else:
-  calcOpFairness(sys.argv[1]) 
+  calcLinearization(sys.argv[1], sys.argv[2], sys.argv[3]) 
